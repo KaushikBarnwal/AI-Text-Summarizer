bda9f640a9a80bf4a125e90eb9440fdc2ef3a5fb
Bishwarup-Das <bishwarupdas.gts@gmail.com>
2024-05-28 01:57:13 +0530

    Updated

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from transformers import pipeline
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        user_input = request.form['text_to_summarize']
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        summerize = summarizer(user_input, max_length=150, min_length=30, do_sample=False)
+        return render_template('index.html', Summary=summerize)
     return render_template('index.html')
-
-
-
-from transformers import pipeline
-
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-
-ARTICLE = """Climate is sometimes mistaken for weather. But climate is different from weather because it is measured over a long period of time, whereas weather can change from day to day, or from year to year. The climate of an area includes seasonal temperature and rainfall averages, and wind patterns. Different places have different climates. A desert, for example, is referred to as an arid climate because little water falls, as rain or snow, during the year. Other types of climate include tropical climates, which are hot and humid, and temperate climates, which have warm summers and cooler winters.
-"""
-print(summarizer(ARTICLE, max_length=150, min_length=30, do_sample=False))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
