38fb9c40d5b2b5437c5227abe605f31981eb31c2
Bishwarup-Das <bishwarupdas.gts@gmail.com>
2024-05-28 02:25:12 +0530

    UPDATED

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, request, render_template
 from transformers import pipeline
 
@@ -6,15 +5,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    summary = ""
     if request.method == 'POST':
         user_input = request.form['text_to_summarize']
         summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-        summerize = summarizer(user_input, max_length=150, min_length=30, do_sample=False)
-        return render_template('index.html', Summary=summerize)
-    return render_template('index.html')
+        summarized_text = summarizer(user_input, max_length=150, min_length=30, do_sample=False)
+        summary = summarized_text[0]['summary_text']
+    return render_template('index.html', Summary=summary)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
